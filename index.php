f4188f6c1430b5f01e251ca5dcd159e9c9d0aaa6
petidesoftwares <petideenterprise@gmail.com>
2021-09-15 01:26:40 +0100

    style commit

--- index.php
+++ index.php
@@ -6,25 +6,27 @@
 </head>
 <body>
 <h2>BINCOM TEST</h2>
-<div id="states">
+<div class="card">
     <span class="span-link"><a href="views/lga.php">View LGA Result</a></span><br>
     <span class="span-link"><a href="views/upload-poll-result.php">Upload Unit Result</a></span><br>
+</div>
+<div class="card" id="states">
     <h4> Delta States Poll Result</h4>
     <ul id = "state-list"</ul>
 </div>
-<div id = "lgas">
+<div class="card" id = "lgas">
     <h4>Local Governments</h4>
     <ul id="lga-list"></ul>
 </div>
-<div id = "wards">
+<div class="card" id = "wards">
     <h4>Wards in </h4>
     <ul id="ward-list"></ul>
 </div>
-<div id = "units">
+<div class="card" id = "units">
     <h4>Polling Units in </h4>
     <ul id="poll-list"></ul>
 </div>
-<div id = "result">
+<div class="card" id = "result">
     <h4>Result For Units in </h4>
     <div id="result-pane">
         <table>
